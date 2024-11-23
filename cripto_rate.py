14c986f023b211ff3bbe783e8e80d88693d9313a
Saratsin-25 <7422037@gmail.com>
2024-11-23 18:24:26 +0300

    Создано окно программы, список популярных криптовалют, комбобокс и метка для ввода выбранной криптовалюты

--- cripto_rate.py
+++ cripto_rate.py
@@ -2,4 +2,33 @@ import requests
 import json
 from tkinter import *
 from tkinter import messagebox as mb
+from tkinter import ttk
 
+
+def update_cr_label():
+    pass
+
+
+cripto = {
+    "Bitcoin": "BTC",
+    "Ethereum": "ETH",
+    "Ripple": "XRP",
+    "Litecoin": "LTC",
+    "Cardano": "ADA",
+    }
+
+
+window = Tk()
+window.title("Обменные курсы криптовалют")
+window.geometry("400x400")
+
+Label(text="Выберите криптовалюту").pack(padx=10, pady=10)
+cr_combobox = ttk.Combobox(values=list(cripto.keys()))
+cr_combobox.pack(padx=10, pady=10)
+
+cr_combobox.bind("<<ComboboxSelected>>", update_cr_label)
+
+cr_label = ttk.Label()
+cr_label.pack(padx=10, pady=10)
+
+window.mainloop()
